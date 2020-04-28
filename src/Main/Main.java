faa5b6ec75f5b7c50154aee3776d3415333d07ad
EduardoBinns <57016116+EduardoBinns@users.noreply.github.com>
2020-04-28 15:27:49 -0600

    cambios basicos

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,8 @@
  */
 package Main;
 
+import Estructura.Arbol;
+
 /**
  *
  * @author Sebas
@@ -15,7 +17,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Arbol arbol = new Arbol();
+      
+        arbol.insertar(2, null);
     }
     
 }
